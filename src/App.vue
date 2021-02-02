d6ada67c2826dc718ebbc983b13aebe6c6b9f652
BryanNilsen <bryannilsen@gmail.com>
2021-02-02 13:08:33 -0600

    completed routing chapter

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
     <v-app-bar app color="teal darken-2" dark>
       <h1>Vue Meme Forum</h1>
       <v-spacer></v-spacer>
-      <router-link to="/"> Home </router-link>
-      <router-link to="/create"> Create </router-link>
-      <router-link to="/feed"> Memes </router-link>
+      <router-link to="/"> <v-btn text>Home</v-btn> </router-link>
+      <router-link to="/create"> <v-btn text>Create</v-btn> </router-link>
+      <router-link to="/feed"> <v-btn text>Memes</v-btn> </router-link>
     </v-app-bar>
 
     <v-main>
-      <router-view></router-view>
+      <transition name="slide-fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </v-main>
   </v-app>
 </template>
@@ -20,5 +22,16 @@ a {
 }
 .router-link-active .v-btn {
   color: rgb(255, 200, 100);
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.3s linear;
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(50px);
+  opacity: 0;
 }
 </style>
